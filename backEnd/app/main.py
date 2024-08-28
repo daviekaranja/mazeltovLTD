c72d94d7a311fdffe72cc777d67828fb1df896a4
Davie Karanja <oonlinegigz@gmail.com>
2024-08-28 14:53:15 +0300

    fixed cors policy

--- backEnd/app/main.py
+++ backEnd/app/main.py
@@ -4,14 +4,14 @@ from .db.session import engine
 from .db.base import Base
 from .api.api import api_router
 from fastapi.middleware.cors import CORSMiddleware
-
+from .core.config import settings
 app = FastAPIOffline()
 app.include_router(api_router)
 
 # Allow CORS for specific origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # List of allowed origins
+    allow_origins=[settings.frontend_origin],  # List of allowed origins
     allow_credentials=True,
     allow_methods=["*"],  # HTTP methods allowed
     allow_headers=["*"],  # HTTP headers allowed
