141ad3ed38e4a2de89d3d01ea178307ebd0ea73d
Davie Karanja <oonlinegigz@gmail.com>
2024-09-17 14:23:28 +0300

    fixed imports

--- backEnd/app/api/api.py
+++ backEnd/app/api/api.py
@@ -4,7 +4,7 @@ from .routes import products, users, login, buy_services
 api_router = APIRouter()
 
 api_router.include_router(login.router, prefix='/auth', tags=['Authentication'])
-api_router.include_router(buy_airtime.router, prefix='/airtime', tags=['Airtime'])
+api_router.include_router(buy_services.router, prefix='/airtime', tags=['Airtime'])
 api_router.include_router(users.router, prefix='/users', tags=['Users'])
 api_router.include_router(products.router, prefix='/products', tags=['Products'])
 
