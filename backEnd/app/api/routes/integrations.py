a8aa413cb3d670c378c4cc471b5c2fcca10446d2
Davie Karanja <oonlinegigz@gmail.com>
2024-09-23 11:19:53 +0300

    log callback url

--- backEnd/app/api/routes/integrations.py
+++ backEnd/app/api/routes/integrations.py
@@ -3,6 +3,7 @@ import requests
 from app.core.config import settings
 from app.utilities.utils import get_timestamp, get_mpesa_token, generate_password
 from app.schemas.payments import PayBillPush, TillPush
+from app.utilities.logger import log
 
 router = APIRouter()
 
@@ -34,6 +35,7 @@ def stk_push(params: PayBillPush, request: Request):
     response = requests.post(api_url, json=payload, headers=headers)
     if response.status_code != 200:
         error_details = response.json()
+        log.info(error_details)
         error_message = error_details.get("errorMessage", "Unknown error occurred")
         raise HTTPException(status_code=response.status_code, detail=error_message)
 
@@ -76,6 +78,6 @@ def stk_push(params: TillPush, request: Request):
 
 @router.post("/mpesa-callback", name="mpesa_callback")
 def mpesa_callback(data: dict):
-    print(data)
+    log.info(f'Received: {data}')
     # Process the callback data here
     return {"message": "Callback received successfully"}
