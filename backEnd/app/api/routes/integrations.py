120f758463810387edd0b25f62e828ed85cf60f1
Davie Karanja <oonlinegigz@gmail.com>
2024-09-26 16:54:00 +0300

    fixed callback url returning a tuple instead of a string

--- backEnd/app/api/routes/integrations.py
+++ backEnd/app/api/routes/integrations.py
@@ -8,39 +8,40 @@ from app.utilities.logger import log
 router = APIRouter()
 
 
-@router.post("/paybill-push", status_code=200)
-def stk_push(params: PayBillPush, request: Request):
-    api_url = settings.api_url
-    headers = {
-        "Authorization": f"Bearer {get_mpesa_token()}",
-        "Content-Type": "application/json"
-    }
-    callback_url = request.url_for("mpesa_callback")
-
-    payload = {
-        "BusinessShortCode": settings.shortcode,
-        "Password": generate_password(),  # Generated password for authentication
-        "Timestamp": get_timestamp(),
-        "TransactionType": 'CustomerPayBillOnline',  # Based on Paybill or Till
-        "Amount": params.amount,
-        "PartyA": int(params.stkNumber),
-        "PartyB": settings.shortcode,  # Same BusinessShortCode for PartyB
-        "PhoneNumber": int(params.stkNumber),
-        "CallBackURL": str(callback_url),
-        "AccountReference": params.rechargeNumber,
-        "TransactionDesc": "Airtime"
-    }
-
-    # Send the STK Push request
-    response = requests.post(api_url, json=payload, headers=headers)
-    if response.status_code != 200:
-        error_details = response.json()
-        log.info(error_details)
-        error_message = error_details.get("errorMessage", "Unknown error occurred")
-        raise HTTPException(status_code=response.status_code, detail=error_message)
-
-    return {"message": "STK Push initiated", "response": response.json()}
-
+#
+# @router.post("/paybill-push", status_code=200)
+# def stk_push(params: PayBillPush, request: Request):
+#     api_url = settings.api_url
+#     headers = {
+#         "Authorization": f"Bearer {get_mpesa_token()}",
+#         "Content-Type": "application/json"
+#     }
+#     callback_url = request.url_for("mpesa_callback")
+#
+#     payload = {
+#         "BusinessShortCode": settings.shortcode,
+#         "Password": generate_password(),  # Generated password for authentication
+#         "Timestamp": get_timestamp(),
+#         "TransactionType": 'CustomerPayBillOnline',  # Based on Paybill or Till
+#         "Amount": params.amount,
+#         "PartyA": int(params.stkNumber),
+#         "PartyB": settings.shortcode,  # Same BusinessShortCode for PartyB
+#         "PhoneNumber": int(params.stkNumber),
+#         "CallBackURL": str(callback_url),
+#         "AccountReference": params.rechargeNumber,
+#         "TransactionDesc": "Airtime"
+#     }
+#
+#     # Send the STK Push request
+#     response = requests.post(api_url, json=payload, headers=headers)
+#     if response.status_code != 200:
+#         error_details = response.json()
+#         log.info(error_details)
+#         error_message = error_details.get("errorMessage", "Unknown error occurred")
+#         raise HTTPException(status_code=response.status_code, detail=error_message)
+#
+#     return {"message": "STK Push initiated", "response": response.json()}
+#
 
 @router.post("/stk-push", status_code=200, response_model=STKPushResponse)
 def stk_push(params: TillPush, request: Request):
@@ -49,7 +50,9 @@ def stk_push(params: TillPush, request: Request):
         "Authorization": f"Bearer {get_mpesa_token()}",
         "Content-Type": "application/json"
     }
-    callback_url = request.url_for('c2b-callback'),
+
+    callback_url = request.url_for('c2b-callback')
+    log.info(f'Testing CallbackUrl: {callback_url}')
 
     payload = {
         "BusinessShortCode": settings.shortcode,  #update to a till
@@ -64,13 +67,12 @@ def stk_push(params: TillPush, request: Request):
         "AccountReference": params.rechargeNumber,
         "TransactionDesc": "Data Deals"
     }
-
+    log.info(f"Payload: \n {payload}")
     # Send the STK Push request
     response = requests.post(api_url, json=payload, headers=headers)
     log.info(response.json())
 
     if response.status_code != 200:
-
         error_details = response.json()
         error_message = error_details.get("errorMessage", "Unknown error occurred")
         raise HTTPException(status_code=response.status_code, detail=error_message)
@@ -80,7 +82,6 @@ def stk_push(params: TillPush, request: Request):
 
 @router.get("/stk-push-query/{checkout_request_id}", status_code=200)
 async def stk_push_query(checkout_request_id: str):
-
     headers = {
         "Authorization": f"Bearer {get_mpesa_token()}",
         "Content-Type": "application/json"
