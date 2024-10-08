bbf197cb4aa169419462d6085689942a8c549b99
Davie Karanja <oonlinegigz@gmail.com>
2024-10-08 17:01:38 +0300

    updated login component

--- frontEnd/src/components/Login.jsx
+++ frontEnd/src/components/Login.jsx
@@ -7,13 +7,21 @@ import {
   FormLabel,
   Text,
   Heading,
+  HStack,
   Input,
   Flex,
   Button,
   Box,
+  StackDivider,
+  Link,
+  InputGroup,
+  InputRightElement,
 } from "@chakra-ui/react";
 
 const Login = () => {
+  const [show, setShow] = React.useState(false);
+  const handleClick = () => setShow(!show);
+
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
@@ -46,50 +54,110 @@ const Login = () => {
   };
 
   return (
-    <Box>
-      <Flex mt={6} direction="column" minH="100vh" alignItems="center">
-        <Heading color={"gray.400"}>Please login to continue</Heading>
-        <Flex
-          direction="column"
-          alignItems="center"
-          p={4}
-          rounded="lg"
-          boxShadow="sm"
-          mx="auto"
+    <Box mt={4} h={"100vh"}>
+      {/* main box */}
+      <Box
+        display={"flex"}
+        flexDirection={"column"}
+        alignItems={"center"}
+        p={4}
+        h={400}
+        rounded={"lg"}
+        width={"70%"}
+        mx={"auto"}
+        mt={4}
+        bg={"white"}
+        border={"1px"}
+        borderColor={"gray.200"}
+      >
+        <HStack
+          w={"100%"}
+          h={"100%"}
+          justifyContent={"center"}
+          p={2}
+          // divider={<StackDivider borderColor={"gray.200"} />}
         >
-          {error && (
-            <Text p={2} color="red.500">
-              {error}
-            </Text>
-          )}
-          <form onSubmit={handleLogin}>
-            <Flex width="100%" gap={4} direction="column">
-              <FormControl isRequired>
-                <FormLabel>Email</FormLabel>
-                <Input
-                  id="username"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                  placeholder="user@example.com"
-                  type="email"
-                />
-              </FormControl>
-              <FormControl isRequired>
-                <FormLabel>Password</FormLabel>
-                <Input
-                  id="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  type="password"
-                />
-              </FormControl>
+          <Box p={2} maxW={"50%"}>
+            <Heading size={"lg"} mt={1}>
+              Mazeltov Commercial <br /> Agencies
+            </Heading>
+            <Text mt={2}>Please Log in to continue</Text>
+          </Box>
+          <Box flex={"1"}>
+            <Flex // input box
+              bg={"white"}
+              direction="column"
+              alignItems="center"
+              rounded="lg"
+              mx="auto"
+            >
+              {error && (
+                <Text p={2} color="red.500">
+                  {error}
+                </Text>
+              )}
+              <form onSubmit={handleLogin}>
+                <Flex width="100%" gap={4} direction="column">
+                  <FormControl bg={"white"} isRequired>
+                    <FormLabel>Email</FormLabel>
+                    <Input
+                      id="username"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                      placeholder="user@example.com"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormControl isRequired>
+                    <FormLabel>Password</FormLabel>
+                    <InputGroup size={"md"}>
+                      <Input
+                        bg="white" // Set background color to white
+                        focusBorderColor="white"
+                        bg={"white"}
+                        type={show ? "text" : "password"}
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                      <InputRightElement pr={2}>
+                        <Button size="sm" onClick={handleClick}>
+                          {show ? "Hide" : "Show"}
+                        </Button>
+                      </InputRightElement>
+                    </InputGroup>
+                  </FormControl>
+                </Flex>
+                <Flex width={"100%"} justifyContent={"center"} p={2}>
+                  <Button
+                    type="submit"
+                    size={"md"}
+                    mt={4}
+                    colorScheme="blue"
+                    bg={"brand.primary"}
+                  >
+                    Login
+                  </Button>
+                </Flex>
+                <Flex mt={2} justifyContent={"space-between"} direction={"row"}>
+                  <Link fontSize={"xs"} color={"brand.primary"}>
+                    create account?
+                  </Link>
+                  <Link fontSize={"xs"} color={"brand.primary"}>
+                    forgot password?
+                  </Link>
+                </Flex>
+              </form>
             </Flex>
-            <Button type="submit" mt={4} width="100%">
-              Login
-            </Button>
-          </form>
-        </Flex>
-      </Flex>
+          </Box>
+        </HStack>
+        <Text color={"gray.400"} fontSize={"xs"}>
+          By using our services you agree to our{" "}
+          <Link fontSize={"xs"} color={"brand.primary"}>
+            Terms and Conditions
+          </Link>
+        </Text>
+      </Box>
     </Box>
   );
 };
